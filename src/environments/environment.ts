19bee866302c6c30f8d45f6a1e7c9f7e67dbce5d
RQureshi <qureshi.r1512@gmail.com>
2019-04-25 22:09:21 +1000

    added code

---
+++ src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+  apiKey: "AIzaSyASJE0obmpGvRGSGWdhlnbtkZX-BYFksUA",
+  authDomain: "angular-ngx-charts.firebaseapp.com",
+  databaseURL: "https://angular-ngx-charts.firebaseio.com",
+  projectId: "angular-ngx-charts",
+  storageBucket: "angular-ngx-charts.appspot.com",
+  messagingSenderId: "61533575740"
+}
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
